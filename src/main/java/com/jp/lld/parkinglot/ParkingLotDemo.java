f6d0d3265de99f6e4b67b0f62f3d081e75aa5f23
Bala Kumar Narayanan <bala850@gmail.com>
2026-02-23 12:05:29 +0530

    Parking lot LLD - test cases corrected

--- src/main/java/com/jp/lld/parkinglot/ParkingLotDemo.java
+++ src/main/java/com/jp/lld/parkinglot/ParkingLotDemo.java
@@ -20,16 +20,16 @@ public class ParkingLotDemo {
 
 
         ParkingFloor floor2 = new ParkingFloor(2);
-        floor1.addSpot(new ParkingSpot("F2-M1", VehicleSize.MEDIUM));
-        floor1.addSpot(new ParkingSpot("F2-M2", VehicleSize.MEDIUM));
-        floor1.addSpot(new ParkingSpot("F2-M3", VehicleSize.MEDIUM));
+        floor2.addSpot(new ParkingSpot("F2-M1", VehicleSize.MEDIUM));
+        floor2.addSpot(new ParkingSpot("F2-M2", VehicleSize.MEDIUM));
+        floor2.addSpot(new ParkingSpot("F2-M3", VehicleSize.MEDIUM));
 
 
 
         ParkingFloor floor3 = new ParkingFloor(3);
-        floor1.addSpot(new ParkingSpot("F3-S1", VehicleSize.SMALL));
-        floor1.addSpot(new ParkingSpot("F3-S2", VehicleSize.SMALL));
-        floor1.addSpot(new ParkingSpot("F3-S3", VehicleSize.SMALL));
+        floor3.addSpot(new ParkingSpot("F3-S1", VehicleSize.SMALL));
+        floor3.addSpot(new ParkingSpot("F3-S2", VehicleSize.SMALL));
+        floor3.addSpot(new ParkingSpot("F3-S3", VehicleSize.SMALL));
 
         parkingLot.addFloor(floor1);
         parkingLot.addFloor(floor2);
@@ -39,6 +39,7 @@ public class ParkingLotDemo {
         System.out.println("Vehicle Entries");
         floor1.displayAvailability();
         floor2.displayAvailability();
+        floor3.displayAvailability();
 
 
         Vehicle bike = new Bike("B-1");
@@ -49,6 +50,17 @@ public class ParkingLotDemo {
         Optional<ParkingTicket> bikeTicket = parkingLot.parkVehicle(bike);
         Optional<ParkingTicket> carTicket = parkingLot.parkVehicle(car);
         Optional<ParkingTicket> truckTicket = parkingLot.parkVehicle(truck);
+
+        if(carTicket.isPresent()) {
+            String licenseNumber = carTicket
+                            .get().getVehicle().getLicenseNumber();
+            Optional<Double> parkingFee =  parkingLot.unparkVehicle(licenseNumber);
+            System.out.println("Fee for "+licenseNumber+": is $"+parkingFee.get());
+        }
+
+        /*if(truckTicket.isPresent()) {
+            parkingLot.unparkVehicle(truckTicket.get().getVehicle().getLicenseNumber());
+        }*/
 
         Vehicle t2 = new Truck("T-2");
         Optional<ParkingTicket> truck2 = parkingLot.parkVehicle(t2);
